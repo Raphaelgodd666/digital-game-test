2aa8ebf605134a686751c402ec764244ad225c5f
Raphaelgodd666 <rafaelali182@gmail.com>
2025-07-24 11:48:54 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+function showMessage() {
+  alert("💌 Happy Birthday! Semoga harimu menyenangkan 🎉");
+}
+
+function showGallery() {
+  alert("📸 Fitur galeri belum ditambahkan. Mau aku bantu isikan?");
+}
+
+function playMusic() {
+  alert("🎵 Musik belum dimulai. Bisa disambungkan ke audio mp3 nanti!");
+}
+
+function playTetris() {
+  alert("🧱 Game Tetris belum aktif. Tapi bisa aku buatkan!");
+}
